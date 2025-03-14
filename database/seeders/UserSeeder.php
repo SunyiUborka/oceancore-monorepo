e76c1b42a1162ee288e84c1d50ad0a9f9c229b1e
SunyiUborka <59057829+SunyiUborka@users.noreply.github.com>
2025-03-14 14:34:11 -0400

     

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,7 +14,7 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         User::create([
-            'name' => 'test',
+            'name' => 'testUser',
             'email' => 'test@test.com',
             'password' => bcrypt('PassWord.123')
         ]);
